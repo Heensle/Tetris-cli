4c098598ca23e47bfa377951684ddaf0ec837162
Heensle <126368145+Heensle@users.noreply.github.com>
2023-06-04 19:51:49 +0000

    Namespaced FallingBlocks class under Block

---
+++ blocks.h
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+namespace Blocks{
+  class FallingBlocks{
+    public:
+      FallingBlocks(string type);
+      void rotate();
+
+    private:
+      string type;
+      string color;
+
+      int block1x;
+      int block1y;
+
+      int block2x;      
+      int block2y;
+
+      int block3x;
+      int block3y;
+
+      int block4x;
+      int block4y;
+
+      int i_rotatex;
+      int i_rotatey;
+  };  
+}
