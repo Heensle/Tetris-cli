a88bed42af1c8a697fb0744d340c717c2e56cf87
Heensle <126368145+Heensle@users.noreply.github.com>
2023-06-05 11:52:39 +0000

    Formatted all

--- graphics.cpp
+++ graphics.cpp
@@ -3,33 +3,31 @@
 using std::cout;
 using std::endl;
 
-void set_screen(){
+void set_screen() {
   keypad(stdscr, TRUE);
   cbreak();
   noecho();
 }
 
-void clear_screen(){
-  cout << "\033[H\033[2J\033[3J";
-}
+void clear_screen() { cout << "\033[H\033[2J\033[3J"; }
 
-void print_header(int points){
+void print_header(int points) {
   cout << "              ┌────┐              " << endl << "              │";
-  if (points < 10){
+  if (points < 10) {
     cout << "  " << points << " ";
-  } else if (points < 100){
+  } else if (points < 100) {
     cout << " " << points << " ";
-  } else if (points < 1000){
+  } else if (points < 1000) {
     cout << " " << points;
-  } else if (points < 10000){
+  } else if (points < 10000) {
     cout << " " << points / 1000 << "k ";
-  } else if (points < 100000){
+  } else if (points < 100000) {
     cout << " " << points / 1000 << "k";
-  } else if (points < 1000000){
+  } else if (points < 1000000) {
     cout << points / 1000 << "k";
-  } else if (points < 10000000){
+  } else if (points < 10000000) {
     cout << " " << points / 1000000 << "m ";
-  } else if (points < 100000000){
+  } else if (points < 100000000) {
     cout << " " << points / 1000000 << "m";
   } else {
     cout << " ok ";
@@ -38,4 +36,3 @@ void print_header(int points){
   cout << "│              " << endl;
   cout << " HOLD ┌───────┴────┴───────┐ NEXT " << endl;
 }
-
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include "blocks.h"
 #include "graphics.h"
 #include <iostream>
 using std::cout;
+using namespace Blocks;
 
 int main() {
 
@@ -11,7 +13,17 @@ int main() {
   while (true) {
     clear_screen();
     print_header(0);
-
+    FallingBlocks piece("I");
+    for (int i = 20; i >= 0; i--) {
+      for (int j = 0; j < 10; j++) {
+        if ((piece.block1x == j && piece.block1y == i) ||
+            (piece.block2x == j && piece.block2y == i) ||
+            (piece.block3x == j && piece.block3y == i) ||
+            (piece.block4x == j && piece.block4y == i)) {
+          cout << "X";
+        }
+      }
+    }
     while (true) {
     }
   }
