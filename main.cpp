c121bef8243a4f0303113cf8929e9190a957e637
Heensle <126368145+Heensle@users.noreply.github.com>
2023-06-07 13:58:02 +0000

    Set up starting piece

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace Blocks;
 
 int rand_num(){
-  return rand() % 5;
+  return rand() % 7 + 1;
 }
 
 int main() {
@@ -21,6 +21,24 @@ int main() {
   double speed;
 
   set_screen();
+
+  int start_piece = rand_num();
+  
+  if (start_piece == 1){
+    FallingBlocks piece("Q");
+  } else if (start_piece == 2){
+    FallingBlocks piece("T");
+  } else if (start_piece == 3){
+    FallingBlocks piece("LL");
+  } else if (start_piece == 4){
+    FallingBlocks piece("LR");
+  } else if (start_piece == 5){
+    FallingBlocks piece("S");
+  } else if (start_piece == 6){
+    FallingBlocks piece("Z");
+  } else if (start_piece == 7){
+    FallingBlocks piece("I");
+  }
 
   while (true) {
     char input = get_input();
